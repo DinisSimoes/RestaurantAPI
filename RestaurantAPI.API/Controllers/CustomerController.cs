ef047a4b8df2e6075be984da45851a4967121f8a
DinisSimoes <di.f.simoes@gmail.com>
2024-11-20 16:41:11 -0300

    create customercontroller

---
+++ RestaurantAPI.API/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Domain.Entities;
+
+namespace RestaurantAPI.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetCustomers([FromQuery] string phoneNumber)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCustomer([FromBody] Customer customer)
+        {
+            return Ok();
+        }
+    }
+}
