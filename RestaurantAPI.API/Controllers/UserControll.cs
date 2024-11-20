e3d23e46e710e0c0265df36564b70714bdadcc45
DinisSimoes <di.f.simoes@gmail.com>
2024-11-20 16:40:58 -0300

    create usercontroll

---
+++ RestaurantAPI.API/Controllers/UserControll.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Domain.Entities;
+
+namespace RestaurantAPI.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] User user)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            return Ok();
+        }
+    }
+}
