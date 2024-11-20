611db2f93c3dcbe28b0dbae685e97fb56119a239
DinisSimoes <di.f.simoes@gmail.com>
2024-11-20 16:22:38 -0300

    create controller

---
+++ RestaurantAPI.API/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Application.Services;
+using RestaurantAPI.Domain.Entities;
+
+namespace RestaurantAPI.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] User user, [FromQuery] string password)
+        {
+            var result = await _authService.RegisterUserAsync(user, password);
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            return Ok("User registered successfully");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] User user, [FromQuery] string password)
+        {
+            var token = await _authService.AuthenticateUserAsync(user.UserName, password);
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(new { Token = token });
+        }
+    }
+}
