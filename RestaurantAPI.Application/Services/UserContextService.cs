da764bfc1c1920ce62f99065793dc32aa18c9f73
DinisSimoes <di.f.simoes@gmail.com>
2024-12-14 18:08:37 -0300

    create service usercontextservice

---
+++ RestaurantAPI.Application/Services/UserContextService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestaurantAPI.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Application.Services
+{
+    public class UserContextService : IUserContextService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserContextService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetCurrentUserId()
+        {
+            var user = _httpContextAccessor.HttpContext?.User;
+            return user?.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "Unknown";
+        }
+    }
+}
