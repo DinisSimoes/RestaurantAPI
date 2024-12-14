7985683fe5624eb3789b0a31a456bcc8e8b9e7b0
DinisSimoes <di.f.simoes@gmail.com>
2024-12-14 18:02:16 -0300

    create menu item with CQRS

---
+++ RestaurantAPI.Application/Commands/MenuItem/CreateMenuItem/CreateMenuItemCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using RestaurantAPI.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Application.Commands.MenuItem.CreateMenuItem
+{
+    public class CreateMenuItemCommand : IRequest<CreateMenuItemResponse>
+    {
+        public MenuItemDto MenuItem { get; set; }
+    }
+}
---
+++ RestaurantAPI.Application/Commands/MenuItem/CreateMenuItem/CreateMenuItemHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using RestaurantAPI.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Application.Commands.MenuItem.CreateMenuItem
+{
+    internal class CreateMenuItemHandler : IRequestHandler<CreateMenuItemCommand, CreateMenuItemResponse>
+    {
+        private readonly IMenuItemService _menuItemService;
+
+        public CreateMenuItemHandler(IMenuItemService menuItemService)
+        {
+            _menuItemService = menuItemService;
+        }
+
+        public async Task<CreateMenuItemResponse> Handle(CreateMenuItemCommand request, CancellationToken cancellationToken)
+        {
+            // Verifique se o menuItem é válido
+            if (request.MenuItem == null)
+            {
+                return new CreateMenuItemResponse
+                {
+                    Success = false,
+                    Message = "MenuItem data is required."
+                };
+            }
+
+            await _menuItemService.AddAsync(request.MenuItem);
+
+            return new CreateMenuItemResponse
+            {
+                Success = true,
+                Message = "MenuItem created successfully."
+            };
+        }
+    }
+}
