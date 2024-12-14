a6cc8f30a30b9cdc50d790828374f49a842abf7a
DinisSimoes <di.f.simoes@gmail.com>
2024-12-14 18:03:57 -0300

    delete item order with CQRS

---
+++ RestaurantAPI.Application/Commands/OrderItem/DeleteItemFromOrder/DeleteItemFromOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Application.Commands.OrderItem.DeleteItemFromOrder
+{
+    public class DeleteItemFromOrderCommand : IRequest<DeleteItemFromOrderResponse>
+    {
+        public Guid OrderId { get; set; }
+        public Guid ItemId { get; set; }
+    }
+}
---
+++ RestaurantAPI.Application/Commands/OrderItem/DeleteItemFromOrder/DeleteItemFromOrderHandler.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using RestaurantAPI.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Application.Commands.OrderItem.DeleteItemFromOrder
+{
+    public class DeleteItemFromOrderHandler : IRequestHandler<DeleteItemFromOrderCommand, DeleteItemFromOrderResponse>
+    {
+        private readonly IOrderService _orderService;
+
+        public DeleteItemFromOrderHandler(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public async Task<DeleteItemFromOrderResponse> Handle(DeleteItemFromOrderCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _orderService.RemoveItemFromOrderAsync(request.OrderId, request.ItemId);
+                return new DeleteItemFromOrderResponse
+                {
+                    Success = true,
+                    Message = "Item removed from the order successfully."
+                };
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return new DeleteItemFromOrderResponse
+                {
+                    Success = false,
+                    Message = ex.Message
+                };
+            }
+            catch (Exception ex)
+            {
+                return new DeleteItemFromOrderResponse
+                {
+                    Success = false,
+                    Message = $"An internal error occurred: {ex.Message}"
+                };
+            }
+        }
+    }
+}
