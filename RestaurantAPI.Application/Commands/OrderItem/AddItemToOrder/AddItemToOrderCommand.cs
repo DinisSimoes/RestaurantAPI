3c58d2a6500e802414e228b89aacc6fdcf649d3a
DinisSimoes <di.f.simoes@gmail.com>
2024-12-14 18:03:39 -0300

    add item order with CQRS

---
+++ RestaurantAPI.Application/Commands/OrderItem/AddItemToOrder/AddItemToOrderCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using RestaurantAPI.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Application.Commands.OrderItem.AddItemToOrder
+{
+    public class AddItemToOrderCommand : IRequest<AddItemToOrderResponse>
+    {
+        public Guid OrderId { get; set; }
+        public OrderItemDto OrderItem { get; set; }
+    }
+}
---
+++ RestaurantAPI.Application/Commands/OrderItem/AddItemToOrder/AddItemToOrderHandler.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using RestaurantAPI.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Application.Commands.OrderItem.AddItemToOrder
+{
+    public class AddItemToOrderHandler : IRequestHandler<AddItemToOrderCommand, AddItemToOrderResponse>
+    {
+        private readonly IOrderService _orderService;
+
+        public AddItemToOrderHandler(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public async Task<AddItemToOrderResponse> Handle(AddItemToOrderCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _orderService.AddItemToOrderAsync(request.OrderId, request.OrderItem);
+
+                return new AddItemToOrderResponse
+                {
+                    Success = true,
+                    Message = "Item added to the order successfully."
+                };
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return new AddItemToOrderResponse
+                {
+                    Success = false,
+                    Message = ex.Message
+                };
+            }
+            catch (Exception ex)
+            {
+                return new AddItemToOrderResponse
+                {
+                    Success = false,
+                    Message = $"An internal error occurred: {ex.Message}"
+                };
+            }
+        }
+    }
+}
